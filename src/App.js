7126c36702415945e9aa61044cde7f1cf9b47517
Fernando Fernandes <fernando.fernandes.opah.com.br>
2023-01-27 15:19:50 -0300

    alterando

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 
 
 function App() {
-  const [count, setCount] = useState(6);
+  const [count, setCount] = useState(28);
+  const [color, setColors] = useState('blue');
+
+ 
+  const setCor = useCallback(() => {
+    count >= 28 ? setColors('red') : setColors('blue')
+  }, [count]);
+
+
+  useEffect(() => {
+    setCor()
+    
+  });
+
+
 
   return (
-    <div className="App">
-      <button onClick={()=>setCount(count + 1)}>+</button>
-      <button onClick={()=>setCount(count - 1)}>-</button>
-      <button onClick={()=>setCount(6)}>resetar</button>
+    <div style={{backgroundColor: color}}>
+      <button onClick={()=>setCount(count + 5)}>+</button>
+      <button onClick={()=>setCount(count - 5)}>-</button>
+      <button onClick={()=>setCount(28)}>resetar</button>
+      {count >= 0 ?<div>positivo</div> : <div>negativo</div>}
       {count}
-   
     </div>
   );
 }
